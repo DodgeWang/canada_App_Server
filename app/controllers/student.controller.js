17d2d2534791c0fb0c95699ee084832cd5744065
DodgeWang <453831794@qq.com>
2017-07-03 01:11:07 +0800

    '修改'

--- app/controllers/student.controller.js
+++ app/controllers/student.controller.js
@@ -37,3 +37,43 @@ exports.getInfo = function(req, res, next) {
     })
 
 }
+
+
+/**
+ * 通过学生Id查询学生的成绩单
+ * @param  {object}   req  the request object
+ * @param  {object}   res  the response object
+ * @param  {Function} next the next func
+ * @return {null}        
+ */
+ exports.getMarkList = function(req, res, next){
+    if (!req.query.studentId) return res.json(resUtil.generateRes(null, config.statusCode.STATUS_INVAILD_PARAMS));
+    var stuId = Number(req.query.studentId);
+    student.getInfo(stuId, function(err, obj) {
+        if (err) {
+            return res.json(resUtil.generateRes(null, config.statusCode.SERVER_ERROR));
+        }
+        var data = null;
+        if (obj !== null) {
+            console.log(obj)
+            data={
+                accumulatedCredit: obj.accumulated_credit, //已得积分
+                lessonList: [{
+                    pCode:obj.p1,
+                    pName:obj.p1_name,
+                    pMark:obj.p1_mark,
+                },{
+                    pCode:obj.p2,
+                    pName:obj.p2_name,
+                    pMark:obj.p2_mark,
+                },{
+                    pCode:obj.p3,
+                    pName:obj.p3_name,
+                    pMark:obj.p3_mark,
+                }]
+            }
+        }
+        res.json(resUtil.generateRes(data, config.statusCode.STATUS_OK));
+    })
+
+ }
--- config/routers.js
+++ config/routers.js
@@ -5,4 +5,5 @@ var student = require('../app/controllers/student.controller');
 module.exports = function(app) {
     app.get('/user/list',user.getList); //获取用户列表
     app.get('/api/studentInfo',student.getInfo); //获取学生基本信息
+    app.get('/api/studentMark',student.getMarkList); //获取学生基本信息
 }
